859711851271caf6fab6b74909f3d198ea812dd6
cypress <motolegacy@proton.me>
2024-09-04 19:37:40 -0700

    common.c: Remove useless zlib include

--- source/common.c
+++ source/common.c
@@ -20,7 +20,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // common.c -- misc functions used in client and server
 
 #include "quakedef.h"
-#include <zlib.h>
 
 #define NUM_SAFE_ARGVS  7
 
