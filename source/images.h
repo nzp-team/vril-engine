df120b6cb8a7e8b57d8aa4cd85662c1279fae735
Tyler Young <tyleryoung88@yahoo.com>
2025-04-06 20:52:39 -0400

    Unity image loading and server.h header

--- source/images.h
+++ source/images.h
@@ -1,4 +1,11 @@
-byte* loadimagepixels (char* filename, qboolean complain, int matchwidth, int matchheight);
-int loadtextureimage (char* filename, int matchwidth, int matchheight, qboolean complain, int filter, qboolean keep, qboolean mipmap);
+byte* Image_LoadPixels (char* filename, int image_format);
+int Image_LoadImage(char* filename, int image_format, int filter, qboolean keep, qboolean mipmap);
 int loadskyboxsideimage (char* filename, int matchwidth, int matchheight, qboolean complain, int filter);
 int loadrgbafrompal (char* name, int width, int height, byte* data);
+void tex_filebase (char *in, char *out);
+
+#define IMAGE_PCX   1
+#define IMAGE_TGA   2
+#define IMAGE_PNG   4
+#define IMAGE_JPG   8
+
