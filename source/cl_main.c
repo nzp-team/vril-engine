nzp-team/vril-engine

commit 0f06f983bb4b3c9dba697e6a3f168d9b349749d5

3DS: Fix many warnings, compile with -Wdouble-promotion

source/cl_main.c
@@ -295,7 +295,11 @@ void CL_PrintEntities_f (void)
 			continue;
 		}
 		Con_Printf ("%s:%2i  (%5.1f,%5.1f,%5.1f) [%5.1f %5.1f %5.1f]\n"
-		,ent->model->name,ent->frame, ent->origin[0], ent->origin[1], ent->origin[2], ent->angles[0], ent->angles[1], ent->angles[2]);
+		,ent->model->name,ent->frame,
+		(double)ent->origin[0], (double)ent->origin[1], (double)ent->origin[2],
+		(double)ent->angles[0],
+		(double)ent->angles[1],
+		(double)ent->angles[2]);
 	}
 }
 
@@ -411,7 +415,7 @@ dlight_t *CL_AllocDlight (int key)
 	dl = cl_dlights;
 	for (i=0 ; i<MAX_DLIGHTS ; i++, dl++)
 	{
-		if (dl->die < cl.time)
+		if (dl->die < (float)cl.time)
 		{
 			memset (dl, 0, sizeof(*dl));
 			dl->key = key;
@@ -434,7 +438,7 @@ void CL_NewDlight (int key, vec3_t origin, float radius, float time, int type)
 	dl = CL_AllocDlight (key);
 	VectorCopy (origin, dl->origin);
 	dl->radius = radius;
-	dl->die = cl.time + time;
+	dl->die = (float)cl.time + time;
 	dl->type = type;
 
 }
@@ -456,7 +460,7 @@ void CL_DecayLights (void)
 	dl = cl_dlights;
 	for (i=0 ; i<MAX_DLIGHTS ; i++, dl++)
 	{
-		if (dl->die < cl.time || !dl->radius)
+		if (dl->die < (float)cl.time || !dl->radius)
 			continue;
 
 		dl->radius -= time*dl->decay;
@@ -486,28 +490,28 @@ float CL_LerpPoint (void)
 		return 1;
 	}
 
-	if (f > 0.1)
+	if (f > 0.1f)
 	{	// dropped packet, or start of demo
 		cl.mtime[1] = cl.mtime[0] - 0.1;
-		f = 0.1;
+		f = 0.1f;
 	}
 
-	frac = (cl.ctime - cl.mtime[1]) / f;
+	frac = (float)(cl.ctime - cl.mtime[1]) / f;
 	if (frac < 0)
 	{
-		if (frac < -0.01)
+		if (frac < -0.01f)
 		{
 			cl.ctime = cl.time = cl.mtime[1];
 		}
-		frac = 0;
+		frac = 0.0f;
 	}
 	else if (frac > 1)
 	{
-		if (frac > 1.01)
+		if (frac > 1.01f)
 		{
 			cl.ctime = cl.time = cl.mtime[0];
 		}
-		frac = 1;
+		frac = 1/-f;
 	}
 
 	return frac;
@@ -523,7 +527,7 @@ vec3_t 	mdlflag_poweruprotate_startangles;
 vec3_t 	mdlflag_poweruprotate_differenceangles;
 vec3_t 	mdlflag_poweruprotate_currentangles;
 
-double 	last_puframetime = 0.0f;
+double 	last_puframetime = 0.0;
 
 /*
 ===============
@@ -535,9 +539,9 @@ void CL_UpdatePowerUpAngles (void)
 	// Don't update more than once per frame.
 	if (last_puframetime != host_frametime) {
 		// New cycle, dictate new rotation time and target angle. 
-		if (mdlflag_poweruprotate_duration <= cl.time) {
+		if (mdlflag_poweruprotate_duration <= (float)cl.time) {
 			mdlflag_poweruprotate_starttime = cl.time;
-			mdlflag_poweruprotate_duration = cl.time + (float)((rand() % 25 + 25)/10.0f); // Take between 2.5 and 5 seconds.
+			mdlflag_poweruprotate_duration = (float)cl.time + (float)((rand() % 25 + 25)/10.0f); // Take between 2.5 and 5 seconds.
 
 			mdlflag_poweruprotate_startangles[0] = mdlflag_poweruprotate_currentangles[0];
 			mdlflag_poweruprotate_startangles[1] = mdlflag_poweruprotate_currentangles[1];
@@ -557,11 +561,11 @@ void CL_UpdatePowerUpAngles (void)
 				if (mdlflag_poweruprotate_currentangles[i] > target_angles[i])
 					mdlflag_poweruprotate_differenceangles[i] = (mdlflag_poweruprotate_currentangles[i] - target_angles[i]) * -1;
 				else
-					mdlflag_poweruprotate_differenceangles[i] = fabs(mdlflag_poweruprotate_currentangles[i] - target_angles[i]);
+					mdlflag_poweruprotate_differenceangles[i] = fabsf(mdlflag_poweruprotate_currentangles[i] - target_angles[i]);
 			}
 		}
 
-		float percentage_complete = (cl.time - mdlflag_poweruprotate_starttime) / (mdlflag_poweruprotate_duration - mdlflag_poweruprotate_starttime);
+		float percentage_complete = ((float)cl.time - mdlflag_poweruprotate_starttime) / (mdlflag_poweruprotate_duration - mdlflag_poweruprotate_starttime);
 
 		for(int j = 0; j < 2; j++) {
 			mdlflag_poweruprotate_currentangles[j] = mdlflag_poweruprotate_startangles[j] + (mdlflag_poweruprotate_differenceangles[j] * percentage_complete);
@@ -977,9 +981,9 @@ int CL_ReadFromServer (void)
 		//{
 			//Con_Printf("++++++++++++++Got a new server message!+++++++++\n");
 		//}
-			cl.last_received_message = realtime;
+		cl.last_received_message = realtime;
 			//Con_Printf("ParseServerMessage \n");
-			CL_ParseServerMessage ();
+		CL_ParseServerMessage ();
 		//}
 
 	} while (ret && cls.state == ca_connected);
@@ -1017,8 +1021,8 @@ void CL_SendCmd (void)
 		CL_BaseMove (&cmd);
 
 	// allow mice or other external controllers to add to the move
-	if (!in_disable_analog.value)
-		IN_Move (&cmd);
+		if (!in_disable_analog.value)
+			IN_Move (&cmd);
 
 	// send the unreliable message
 		CL_SendMove (&cmd);

Makefile.ctr
@@ -26,7 +26,7 @@ APP_DESCRIPTION := Call of Duty Zombies remake
 #---------------------------------------------------------------------------------
 ARCH	:=	-march=armv6k -mtune=mpcore -mfloat-abi=hard -mtp=soft
 
-CFLAGS	:=	-g -fpermissive -Wall -O3 -mword-relocations \
+CFLAGS	:=	-g -Wall -Wdouble-promotion -O3 -mword-relocations \
 			-fomit-frame-pointer -ffunction-sections \
 			$(ARCH)
 

source/chase.c
@@ -171,9 +171,9 @@ void Chase_Update (void)
       dist = 1;
 
    #ifdef PSP_VFPU
-   r_refdef.viewangles[PITCH] = -vfpu_atanf(stop[2] / dist) / M_PI * 180;
+   r_refdef.viewangles[PITCH] = -vfpu_atanf(stop[2] / dist) * ((float)M_PI / 180.0f);
    #else
-   r_refdef.viewangles[PITCH] = -atan(stop[2] / dist) / M_PI * 180;
+   r_refdef.viewangles[PITCH] = -atanf(stop[2] / dist) * ((float)M_PI / 180.0f);
    #endif
 
    // move towards destination

source/cl_demo.c
@@ -195,7 +195,7 @@ record <demoname> <map> [cd track]
 void CL_Record_f (void)
 {
 	int		c;
-	char	name[MAX_OSPATH];
+	char	name[MAX_OSPATH+1];
 	int		track;
 	char	forcetrack[16];
 
@@ -229,8 +229,10 @@ void CL_Record_f (void)
 	}
 	else
 		track = -1;	
-
-	snprintf (name, MAX_OSPATH + 1, "%s/%s", com_gamedir, Cmd_Argv(1));
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat-truncation"
+	snprintf (name, MAX_OSPATH, "%s/%s", com_gamedir, Cmd_Argv(1));
+#pragma GCC diagnostic pop
 	
 //
 // start the map up
@@ -344,7 +346,7 @@ void CL_FinishTimeDemo (void)
 	
 // the first frame didn't count
 	frames = (host_framecount - cls.td_startframe) - 1;
-	time = realtime - cls.td_starttime;
+	time = realtime - (double)cls.td_starttime;
 	if (time < 1)
 		time = 1;
 	Con_Printf ("%i frames %5.1f seconds %5.1f fps\n", frames, time, frames/time);

source/cl_hud.c
@@ -280,8 +280,8 @@ void HUD_NewMap (void)
 	point_change_interval = 0;
 	point_change_interval_neg = 0;
 
-	round_center_x = (vid.width - sb_round[0]->width) /2;
-	round_center_y = (vid.height - sb_round[0]->height) /2;
+	round_center_x = (vid.width - sb_round[0]->width) / 2.0f;
+	round_center_y = (vid.height - sb_round[0]->height) / 2.0f;
 	
 	bettyprompt_time = 0;
 	nameprint_time = 0;
@@ -310,8 +310,7 @@ int HUD_itoa (int num, char *buf)
 		num = -num;
 	}
 
-	for (pow10 = 10 ; num >= pow10 ; pow10 *= 10)
-	;
+	for (pow10 = 10 ; num >= pow10 ; pow10 *= 10);
 
 	do
 	{
@@ -477,10 +476,10 @@ void HUD_Parse_Point_Change (int points, int negative, int x_start, int y_start)
 	point_change[i].negative = negative;
 
 	f = HUD_itoa (points, str);
-	point_change[i].x = x_start + (10.0 + 8.0*f * hud_scale_factor);
+	point_change[i].x = x_start + (10.0f + 8.0f*f * hud_scale_factor);
 	point_change[i].y = y_start;
-	point_change[i].move_x = (1.0 * hud_scale_factor);
-	point_change[i].move_y = ((rand ()&0x7fff) / ((float)0x7fff)) - (0.5 * hud_scale_factor);
+	point_change[i].move_x = (1.0f * hud_scale_factor);
+	point_change[i].move_y = ((rand ()&RAND_MAX) / ((float)RAND_MAX)) - (0.5f * hud_scale_factor);
 
 	point_change[i].alive_time = Sys_FloatTime() + 0.4;
 }
@@ -609,9 +608,9 @@ void HUD_Blood (void)
 	//this function scales linearly from health = 0 to health = 100
 	//alpha = (100.0 - (float)cl.stats[STAT_HEALTH])/100*255;
 	//but we want the picture to be fully visible at health = 20, so use this function instead
-	alpha = (100.0 - ((1.25 * (float) cl.stats[STAT_HEALTH]) - 25))/100*255;
+	alpha = (100.0f - ((1.25f * cl.stats[STAT_HEALTH]) - 25))/100*255;
 	
-	if (alpha <= 0.0)
+	if (alpha <= 0.0f)
 		return;
     
 #ifdef PSP_VFPU
@@ -620,14 +619,14 @@ void HUD_Blood (void)
 	float modifier = (sin(cl.time * 10) * 20) - 20;//always negative
 #endif // PSP_VFPU
 
-	if(modifier < -35.0)
-		modifier = -35.0;
+	if(modifier < -35.0f)
+		modifier = -35.0f;
     
 	alpha += modifier;
     
-	if(alpha < 0.0)
+	if(alpha < 0.0f)
 		return;
-	float color = 255.0 + modifier;
+	float color = 255.0f + modifier;
     
 	Draw_ColoredStretchPic(0, 0, fx_blood_lu, vid.width, vid.height, color, color, color, alpha);
 }
@@ -680,19 +679,19 @@ void HUD_WorldText(int alpha)
 		if (!strcmp("chaptertitle", key)) // search for chaptertitle key
 		{
 			has_chaptertitle = true;
-			Draw_ColoredString(6 * hud_scale_factor, vid.height/2 + (10 * hud_scale_factor), value, 255, 255, 255, alpha, hud_scale_factor);	
+			Draw_ColoredString(6 * hud_scale_factor, vid.height/2.0f + (10 * hud_scale_factor), value, 255, 255, 255, alpha, hud_scale_factor);	
 		}
 		if (!strcmp("location", key)) // search for location key
 		{
-			Draw_ColoredString(6 * hud_scale_factor, vid.height/2 + (20 * hud_scale_factor), value, 255, 255, 255, alpha, hud_scale_factor);
+			Draw_ColoredString(6 * hud_scale_factor, vid.height/2.0f + (20 * hud_scale_factor), value, 255, 255, 255, alpha, hud_scale_factor);
 		}
 		if (!strcmp("date", key)) // search for date key
 		{
-			Draw_ColoredString(6 * hud_scale_factor, vid.height/2 + (30 * hud_scale_factor), value, 255, 255, 255, alpha, hud_scale_factor);
+			Draw_ColoredString(6 * hud_scale_factor, vid.height/2.0f + (30 * hud_scale_factor), value, 255, 255, 255, alpha, hud_scale_factor);
 		}
 		if (!strcmp("person", key)) // search for person key
 		{
-			Draw_ColoredString(6 * hud_scale_factor, vid.height/2 + (40 * hud_scale_factor), value, 255, 255, 255, alpha, hud_scale_factor);
+			Draw_ColoredString(6 * hud_scale_factor, vid.height/2.0f + (40 * hud_scale_factor), value, 255, 255, 255, alpha, hud_scale_factor);
 		}
 	}
 }
@@ -795,7 +794,7 @@ void HUD_Rounds (void)
 		HUD_WorldText(value2);
 		
 		if (has_chaptertitle == false)
-			Draw_ColoredString(6 * hud_scale_factor, vid.height/2 + (10 * hud_scale_factor), "'Nazi Zombies'", 255, 255, 255, value2, hud_scale_factor);
+			Draw_ColoredString(6 * hud_scale_factor, vid.height/2.0f + (10 * hud_scale_factor), "'Nazi Zombies'", 255, 255, 255, value2, hud_scale_factor);
 		
 		value -= cl.time * 0.4;
 		value2 += cl.time * 0.4;
@@ -811,7 +810,7 @@ void HUD_Rounds (void)
 		HUD_WorldText(255);
 		
 		if (has_chaptertitle == false)
-			Draw_ColoredString(6 * hud_scale_factor, vid.height/2 + (10 * hud_scale_factor), "'Nazi Zombies'", 255, 255, 255, 255, hud_scale_factor);
+			Draw_ColoredString(6 * hud_scale_factor, vid.height/2.0f + (10 * hud_scale_factor), "'Nazi Zombies'", 255, 255, 255, 255, hud_scale_factor);
 
 		value2 += cl.time * 0.4;
 
@@ -825,7 +824,7 @@ void HUD_Rounds (void)
 		HUD_WorldText(value2);
 		
 		if (has_chaptertitle == false)
-			Draw_ColoredString(6 * hud_scale_factor, vid.height/2 + (10 * hud_scale_factor), "'Nazi Zombies'", 255, 255, 255, value2, hud_scale_factor);
+			Draw_ColoredString(6 * hud_scale_factor, vid.height/2.0f + (10 * hud_scale_factor), "'Nazi Zombies'", 255, 255, 255, value2, hud_scale_factor);
 
 		value2 -= cl.time * 0.4;
 
@@ -860,8 +859,8 @@ void HUD_Rounds (void)
 	{
 		Draw_ColoredStretchPic(round_center_x, round_center_y, sb_round[0], 
 		sb_round[0]->width * hud_scale_factor, sb_round[0]->height * hud_scale_factor, 107, 1, 0, 255);
-		round_center_x = round_center_x - (229/108) - (0.2 * hud_scale_factor);
-		round_center_y = round_center_y + (0.95 * hud_scale_factor); // don't move y too quickly
+		round_center_x = round_center_x - (229.0f/108.0f) - (0.2f * hud_scale_factor);
+		round_center_y = round_center_y + (0.95f * hud_scale_factor); // don't move y too quickly
 		if (round_center_x <= 5 * hud_scale_factor)
 			round_center_x = 5 * hud_scale_factor;
 		if (round_center_y >= vid.height - (sb_round[0]->height * hud_scale_factor) - 2)
@@ -948,7 +947,7 @@ void HUD_Rounds (void)
 	}
 	else if (cl.stats[STAT_ROUNDCHANGE] == 4)//blink white
 	{
-		if (endroundchange > cl.time) {
+		if (endroundchange > (float)cl.time) {
 			blinking = (((int)(realtime*475)&510) - 255);
 			blinking = abs(blinking);
 		} else {
@@ -1385,13 +1384,13 @@ void HUD_Powerups (void)
 
 	// both are avail draw fixed order
 	if (count == 2) {
-		Draw_StretchPic((vid.width/2) - (27 * hud_scale_factor), vid.height - (29 * hud_scale_factor), x2pic, scale, scale);
-		Draw_StretchPic((vid.width/2) + (3 * hud_scale_factor), vid.height - (29 * hud_scale_factor), instapic, scale, scale);
+		Draw_StretchPic((vid.width/2.0f) - (27 * hud_scale_factor), vid.height - (29 * hud_scale_factor), x2pic, scale, scale);
+		Draw_StretchPic((vid.width/2.0f) + (3 * hud_scale_factor), vid.height - (29 * hud_scale_factor), instapic, scale, scale);
 	} else {
 		if (cl.stats[STAT_X2])
-			Draw_StretchPic((vid.width/2) - (13 * hud_scale_factor), vid.height - (29 * hud_scale_factor), x2pic, scale, scale);
+			Draw_StretchPic((vid.width/2.0f) - (13 * hud_scale_factor), vid.height - (29 * hud_scale_factor), x2pic, scale, scale);
 		if(cl.stats[STAT_INSTA])
-			Draw_StretchPic ((vid.width/2) - (13 * hud_scale_factor), vid.height - (29 * hud_scale_factor), instapic, scale, scale);
+			Draw_StretchPic ((vid.width/2.0f) - (13 * hud_scale_factor), vid.height - (29 * hud_scale_factor), instapic, scale, scale);
 	}
 }
 
@@ -1437,7 +1436,7 @@ void HUD_Achievement (void)
 
 	if (achievement_unlocked == 1)
 	{
-		smallsec = smallsec + 0.7;
+		smallsec = smallsec + 0.7f;
 		if (smallsec >= 55)
 			smallsec = 55;
 		//Background image

source/cl_input.c
@@ -306,7 +306,7 @@ void CL_AdjustAngles (void)
 	float	cl_sensitivity;
 
 	if (in_speed.state & 1)
-		speed = host_frametime * cl_anglespeedkey.value;
+		speed = (float)host_frametime * cl_anglespeedkey.value;
 	else
 		speed = host_frametime;
 
@@ -319,13 +319,13 @@ void CL_AdjustAngles (void)
 	// cut look speed in half when facing enemy, unless
 	// mag is empty
 	if ((in_aimassist.value) && (sv_player->v.facingenemy == 1) && cl.stats[STAT_CURRENTMAG] > 0) {
-		speed *= 0.5;
+		speed *= 0.5f;
 	}
 	// additionally, slice look speed when ADS/scopes
 	if (cl.stats[STAT_ZOOM] == 1)
-		speed *= 0.5;
+		speed *= 0.5f;
 	else if (cl.stats[STAT_ZOOM] == 2)
-		speed *= 0.25;
+		speed *= 0.25f;
 	
 #ifdef __PSP__
 	cl_sensitivity = in_sensitivity.value;
@@ -395,12 +395,12 @@ void CL_BaseMove (usercmd_t *cmd)
 	cl_backspeed = cl_forwardspeed = cl_sidespeed = sv_player->v.maxspeed;
 
 	// Throttle side and back speeds
-	cl_sidespeed *= 0.8;
-	cl_backspeed *= 0.7;
+	cl_sidespeed *= 0.8f;
+	cl_backspeed *= 0.7f;
 	
 	if (waypoint_mode.value)
-		cl_backspeed = cl_forwardspeed = cl_sidespeed *= 1.5;
-		
+		cl_backspeed = cl_forwardspeed = cl_sidespeed *= 1.5f;
+			
 	if (in_strafe.state & 1)
 	{
 		cmd->sidemove += cl_sidespeed * CL_KeyState (&in_right);
@@ -459,7 +459,7 @@ int infront(edict_t *ent1, edict_t *ent2)
 	AngleVectors(temp_angle,temp_forward,temp_right,temp_up);
 
 	dot = DotProduct(vec,temp_forward);
-	if(dot > 0.98)
+	if(dot > 0.98f)
 	{
 		return 1;
 	}
@@ -514,7 +514,7 @@ void CL_Aim_Snap(void)
 
 	if (cl.perks & 64)
     	znum = EN_Find(0,"ai_zombie_head");
-  	else
+	else
     	znum = EN_Find(0,"ai_zombie");
 
 	z = EDICT_NUM(znum);

source/cl_parse.c
@@ -126,12 +126,12 @@ void CL_ParseStartSoundPacket(void)
 
     field_mask = MSG_ReadByte();
 
-    if (field_mask & SND_VOLUME)
+	if (field_mask & SND_VOLUME)
 		volume = MSG_ReadByte ();
 	else
 		volume = DEFAULT_SOUND_PACKET_VOLUME;
 
-    if (field_mask & SND_ATTENUATION)
+	if (field_mask & SND_ATTENUATION)
 		attenuation = MSG_ReadByte () / 64.0;
 	else
 		attenuation = DEFAULT_SOUND_PACKET_ATTENUATION;
@@ -608,7 +608,7 @@ void CL_ParseUpdate (int bits)
 // Tomaz - QC Alpha Scale Glow Begin
 	if (bits & U_RENDERAMT)
 	    ent->renderamt = MSG_ReadFloat();
-    else
+	else
 	    ent->renderamt = 0;
 
     if (bits & U_RENDERMODE)
@@ -618,17 +618,17 @@ void CL_ParseUpdate (int bits)
 
 	if (bits & U_RENDERCOLOR1)
 	    ent->rendercolor[0] = MSG_ReadFloat();
-    else
+	else
 	    ent->rendercolor[0] = 0;
 
 	if (bits & U_RENDERCOLOR2)
 	    ent->rendercolor[1] = MSG_ReadFloat();
-    else
+	else
 	    ent->rendercolor[1] = 0;
 
 	if (bits & U_RENDERCOLOR3)
 	    ent->rendercolor[2] = MSG_ReadFloat();
-    else
+	else
 	    ent->rendercolor[2] = 0;
 // Tomaz - QC Alpha Scale Glow End
 
@@ -1167,7 +1167,7 @@ void CL_ParseServerMessage (void)
 
 		case svc_time:
 			cl.mtime[1] = cl.mtime[0];
-			cl.mtime[0] = MSG_ReadFloat ();
+			cl.mtime[0] = (double)MSG_ReadFloat ();
 			break;
 
 		case svc_clientdata:

source/cl_tent.c
@@ -99,7 +99,7 @@ void CL_ParseBeam (model_t *m)
 // find a free beam
 	for (i=0, b=cl_beams ; i< MAX_BEAMS ; i++, b++)
 	{
-		if (!b->model || b->endtime < cl.time)
+		if (!b->model || b->endtime < (float)cl.time)
 		{
 			b->entity = ent;
 			b->model = m;
@@ -406,7 +406,7 @@ void CL_UpdateTEnts (void)
 // update lightning
 	for (i=0, b=cl_beams ; i< MAX_BEAMS ; i++, b++)
 	{
-		if (!b->model || b->endtime < cl.time)
+		if (!b->model || b->endtime < (float)cl.time)
 			continue;
 
 		// if coming from the player, update the start position
@@ -473,12 +473,12 @@ void CL_UpdateTEnts (void)
 		}
 		else
 		{
-			yaw = (int) (atan2f(dist[1], dist[0]) * 180 / M_PI);
+			yaw = (int) (atan2f(dist[1], dist[0]) * (180 / (float)M_PI));
 			if (yaw < 0)
 				yaw += 360;
 
 			forward = sqrtf (dist[0]*dist[0] + dist[1]*dist[1]);
-			pitch = (int) (atan2f(dist[2], forward) * 180 / M_PI);
+			pitch = (int) (atan2f(dist[2], forward) * (180 / (float)M_PI));
 			if (pitch < 0)
 				pitch += 360;
 		}

commit 221e2986deaf27d3cb25de86ba1a16d3886f6bd6

GLOBAL: Fix typo introduced in #65

source/cl_main.c
@@ -506,7 +506,7 @@ float CL_LerpPoint (void)
 		{
 			cl.ctime = cl.time = cl.mtime[0];
 		}
-		frac = 1/-f;
+		frac = 1;
 	}
 
 	return frac;