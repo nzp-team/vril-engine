2e8394539092318238ef37828b691024ae701acf
MotoLegacy <motolegacy@proton.me>
2025-05-09 19:04:01 -0700

    GLOBAL: Re-organize platform directories (again)

--- source/host.c
+++ source/host.c
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "nzportable_def.h"
 
 #ifdef __PSP__
-#include "platform_psp/thread.h"
+#include "platform/psp/thread.h"
 #include <pspsysevent.h>
 #endif // __PSP__
 
