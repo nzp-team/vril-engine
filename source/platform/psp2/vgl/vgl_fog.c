b1fb5fb43ec69f9c6659886c3253e49d1aa846a2
Fancy2209 <paulo.medeiros1302@gmail.com>
2025-07-22 02:02:33 +0000

    PSP2: Add slightly less broken linear fog

--- source/platform/psp2/vgl/vgl_fog.c
+++ source/platform/psp2/vgl/vgl_fog.c
@@ -31,14 +31,14 @@ extern GLint fograngeuniformlocs[5];
 void
 Platform_Fog_Set(bool is_world_geometry, float start, float end, float red, float green, float blue, float alpha)
 {
-    //float color[4] = {red, green, blue, alpha};
+    //float color[4] = {80, 80, 80, 100};
     //float color[4] = {red/255, green/255, blue/255, alpha};
-    float color[4] = {red*0.1f, green*0.1f, blue*0.1f, alpha};
+    float color[4] = {red / 64.0f, green / 64.0f, blue / 64.0f, alpha};
     for(int i = 0; i < 5; i++)
     {
         glUniform4fv(fogcoloruniformlocs[i], 1, color);
-        glUniform1f(fogfaruniformlocs[i], end);
-        glUniform1f(fograngeuniformlocs[i], end - start);
+        glUniform1f(fogfaruniformlocs[i], end/64.0f);
+        glUniform1f(fograngeuniformlocs[i], end/64.0f - start/64.0f);
     }
 }
 
