648da8a071ef70ac843cb2bbe7f3479a4537fb01
tyleryoung <tyleryoung88@yahoo.com>
2026-05-04 21:20:20 -0400

    NSPIRE: Add include to music.c

--- source/platform/nspire/music.c
+++ source/platform/nspire/music.c
@@ -16,6 +16,8 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
+#include "../../nzportable_def.h"
+
 qboolean                music_paused = false;
 
 volatile int            music_job_started;
